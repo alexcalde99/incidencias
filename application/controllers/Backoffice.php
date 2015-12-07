2da743077e9523c981c71cc7892e5e2d62753c70
alexcalde99 <alexcalde999@gmail.com>
2015-12-07 17:56:07 +0100

    usuarios->ok incidencias->ok roles->ok tipo incidencias->ok

--- application/controllers/Backoffice.php
+++ application/controllers/Backoffice.php
@@ -63,6 +63,58 @@ class Backoffice extends CI_Controller
 
     }
 
+    /******************************************************************************
+     * funcion CRUD incidencias
+     *****************************************************************************/
+
+
+    function crud_incidencias(){
+        //creamos objeto crud
+        $crud = new grocery_CRUD();
+        //Seleccionar la tabla incidencias
+        $crud->set_table('incidencias');
+        //objetener la info de la tabla en un array, para despues mostrar
+        $output = $crud->render();
+
+        //a la vista-backofffice le paso el output(array)
+        $this->load->view('vista-backoffice', $output);
+
+
+    }
+
+    /******************************************************************************
+     * funcion CRUD ROLES
+     *****************************************************************************/
+
+    function crud_roles(){
+        //creamos objeto crud
+        $crud = new grocery_CRUD();
+        //Seleccionar la tabla ROLES
+        $crud->set_table('roles');
+        //obtener la info de la tabla en un array, para despues mostrar
+        $output = $crud->render();
+
+        //a la vista-backoffice le pasamos el output(array)
+        $this->load->view('vista-backoffice', $output);
+
+    }
+
+    /******************************************************************************
+     * funcion CRUD ROLES
+     *****************************************************************************/
+
+    function crud_tipo_incidencias(){
+        //creamos objeto crud
+        $crud = new grocery_CRUD();
+        //Seleccionar la tabla TIPOS INCIDENCIAS
+        $crud->set_table('tipos_incidencias');
+        //obtener la info de la tabla en un array, para despues mostrar
+        $output = $crud->render();
+
+        //a la vista-backoffice le pasamos el output(array)
+        $this->load->view('vista-backoffice', $output);
+
+    }
 
 
     /******************************************************************************
@@ -80,24 +132,9 @@ class Backoffice extends CI_Controller
 
 
 
-    /******************************************************************************
-     * funcion CRUD incidencias
-     *****************************************************************************/
 
 
-    function crud_incidencias(){
-        //creamos objeto crud
-        $crud = new grocery_CRUD();
-        //Seleccionar la tabla usuarios
-        $crud->set_table('incidencias');
-        //objetener la info de la tabla en un array, para despues mostrar
-        $output = $crud->render();
 
-        //a la vista-backofffice le paso el output(array)
-        $this->load->view('vista-backoffice', $output);
-
-
-    }
 
 
     /******************************************************************************
